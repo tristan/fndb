2cb95ea8191af5121b625b6c7d394333855164be
Tristan King <tristan.king@gmail.com>
2013-06-23 06:28:03 +0200

    fixed up setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     version='0.1.0',
     author='Tristan King',
     author_email='tristan.king@gmail.com',
-    packages=['fndb', 'fndb.test'],
+    packages=['fndb','fndb.tests'],
     url='http://github.com/tristan/fndb',
     description='Data Modeling and Querying based off the NDB libraries available on the Google AppEngine.',
     long_description=open('README.md').read(),
     install_requires=[
     ],
-    extra_require = {
+    extras_require = {
         'simplekv_store': ['simplekv']
     }
 )
